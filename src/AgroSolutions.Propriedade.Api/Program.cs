f11133d2cd587f1f6f86a256e371d9bc701a0dac
Ivís Confessor <ivisconfessor@gmail.com>
2026-02-27 11:23:23 -0300

    finalizacao add cors para consumo de dashboard web

--- src/AgroSolutions.Propriedade.Api/Program.cs
+++ src/AgroSolutions.Propriedade.Api/Program.cs
@@ -27,6 +27,16 @@ builder.Services.AddDbContext<PropriedadeDbContext>(options =>
     options.UseNpgsql(connectionString);
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Pipeline HTTP
@@ -41,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.MapPost("/propriedades", async (CriarPropriedadeRequest request, PropriedadeDbContext db) =>
 {
